8016ee3690a9c7e18b946e931d53073b453c96dc
AmirFone <ahossain20@fordham.edu>
2023-10-15 16:53:53 -0400

    finally done with p2_c.pu

--- Homework_two/problems/problem_2/p2_c.py
+++ Homework_two/problems/problem_2/p2_c.py
@@ -1,65 +1,34 @@
 import math
 import random
 
-def d(point1, point2):
-    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+# Function to calculate distance
+def get_distance(point1, point2):
+    return math.sqrt(((point1[0]-point2[0])**2) + ((point1[1]-point2[1])**2))
 
 def closest_pair(points):
+    random.seed(1)
+    random.shuffle(points)
 
-    # Brute force method
-    def brute(points):
-        min_distance = float('inf')
-        closest = []
-        for i in range(len(points) - 1):
-            for j in range(i+1, len(points)):
-                distance = d(points[i], points[j])
-                if distance < min_distance:
-                    min_distance = distance
-                    closest = [points[i],points[j]]
-        return closest
+    p1 = points[0]
+    p2 = points[1]
+    delta = get_distance(p1, p2)
+    pair = {tuple(p1), tuple(p2)}
 
-    # Select random pivot for quicksort
-    def random_pivot(pts):
-        pivot = random.choice(pts)
-        below = [point for point in pts if point < pivot]
-        above = [point for point in pts if point > pivot]
-        return below, pivot, above
+    # Use a dict to store bucketed points
+    buckets = {(p1[0]//1, p1[1]//1): {tuple(p1)}, (p2[0]//1, p2[1]//1): {tuple(p2)}}
 
-    # Quicksort
-    def quicksort(pts):
-        if len(pts) <= 1:
-            return pts
-        below, pivot, above = random_pivot(pts)
-        return quicksort(below) + [pivot] + quicksort(above)
+    for point in points[2:]:
+        x = int(point[0]//1); y = int(point[1]//1)
+        indices = [(a, b) for a in [x-1, x, x+1] for b in [y-1, y, y+1]]
+        for dx, dy in indices:
+            if (dx, dy) in buckets:
+                for neighbor in buckets[(dx, dy)]:
+                    d = get_distance(list(neighbor), point)
+                    if d < delta:
+                        delta = d
+                        pair = {tuple(neighbor), tuple(point)}
+        if (x,y) not in buckets:
+            buckets[(x, y)] = set()
+        buckets[(x, y)].add(tuple(point))
 
-    # Sort points by x-coord 
-    points = quicksort(points)
-
-    def divide_conquer(pts):
-
-        if len(pts) <= 3:
-            return brute(pts)
-
-        mid = len(pts) // 2
-        mid_point = pts[mid]
-        
-        left = pts[:mid]
-        right = pts[mid:]
-        
-        closest_pair = min(divide_conquer(left), divide_conquer(right), key = lambda x: d(*x))
-
-        strip = [point for point in pts if abs(point[0] - mid_point[0]) < d(*closest_pair)] 
-
-        strip.sort(key = lambda x: x[1]) # sort by y-coordinate
-        
-        min_distance_so_far = d(*closest_pair)
-        
-        for i, point in enumerate(strip):
-            for j in range(i+1, min(i+7, len(strip))): # Only check next 7 points
-                if strip[j][1] - strip[i][1] < min_distance_so_far:
-                    min_distance_so_far = d(strip[i], strip[j])
-                    closest_pair = (strip[i], strip[j])
-
-        return closest_pair
-
-    return divide_conquer(points)
+    return pair
--- Homework_two/problems/problem_3/p3_b.py
+++ Homework_two/problems/problem_3/p3_b.py
@@ -1,14 +1,9 @@
-'''
-Problem 3b
-'''
-
-import random
-
 class Node: 
     def __init__(self, val, level):
         self.val = val
         self.next = [None] * level
         self.level = level
+
 
 class SkipList:
     def __init__(self, max_level, p):
@@ -25,7 +20,8 @@ class SkipList:
 
     def insert(self, x: int) -> None:
         new_level = self.random_level()
-        new_node = Node(x, new_level)
+        new_node = Node(x, self.max_level)  # change here, create node with max_level pointers
+        
         update = [None] * (self.max_level+1)
         current = self.root
 
@@ -43,39 +39,3 @@ class SkipList:
                 if update[i] is not None:
                     new_node.next[i] = update[i].next[i]
                     update[i].next[i] = new_node
-
-
-    def delete(self, x: int) -> None:
-        update = [None] * (self.max_level + 1)
-        current = self.root
-
-        for i in range(self.max_level, -1, -1):
-            while current.next[i] and current.next[i].val < x:
-                current = current.next[i]
-            update[i] = current
-
-        current = current.next[0]
-
-        if current and current.val == x:
-            # if the node to be deleted is the root
-            if x == self.root.val:
-                # find the next node and make it the new root
-                temp = [node for node in self.root.next if node is not None]
-                self.root = min(temp, key=lambda x: x.val, default=None) if temp else None 
-                
-            for i in range(len(current.next)):
-                if update[i].next[i] != current:
-                    break
-                update[i].next[i] = current.next[i]
-
-    def search(self, x: int) -> Node:
-        current = self.sentinel
-
-        for i in range(self.max_level, -1, -1):
-            while i < len(current.next) and current.next[i] and current.next[i].val < x:
-                current = current.next[i]
-
-        current = current.next[0]
-
-        if current and current.val == x:
-            return current
