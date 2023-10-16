88d984990b724d04d9c32381b39a9e37163f460d
AmirFone <ahossain20@fordham.edu>
2023-10-16 15:00:33 -0400

    committing so I can change around p3_c

---
+++ Homework_two/problems/problem_2/testing.py
@@ -0,0 +1,55 @@
+import random
+import time
+import matplotlib.pyplot as plt
+from p2_a import closest_pair as closest_a
+from p2_b import closest_pair as closest_dp
+from p2_c import closest_pair as closest_rand
+
+def generate_random_coordinates_list(size):
+    coordinates_list = [(random.uniform(0, 100), random.uniform(0, 100)) for _ in range(size)]
+    return coordinates_list
+
+test_case_n = [10, 50, 100, 250, 500,750,1000,1500,2000,2500,100_000]
+
+runtimes_closest = []
+runtimes_closest_dp = []
+runtimes_closest_rand = []
+
+for x in test_case_n:
+    print("Test Case Size:", x)
+    coordinates_list = generate_random_coordinates_list(x)
+    
+    start_time = time.time()
+    closest_a(coordinates_list)
+    end_time = time.time()
+    runtime_closest = end_time - start_time
+    runtimes_closest.append(runtime_closest)
+
+    start_time = time.time()
+    closest_dp(coordinates_list)
+    end_time = time.time()
+    runtime_closest_dp = end_time - start_time
+    runtimes_closest_dp.append(runtime_closest_dp)
+
+    start_time = time.time()
+    closest_rand(coordinates_list)
+    end_time = time.time()
+    runtime_closest_rand = end_time - start_time
+    runtimes_closest_rand.append(runtime_closest_rand)
+
+    print("Closest Pair:", runtime_closest)
+    print("Closest Pair DP:", runtime_closest_dp)
+    print("Closest Pair Random:", runtime_closest_rand)
+    print("------------------------------------------------------------")
+
+plt.figure(figsize=(10, 6))
+plt.plot(test_case_n, runtimes_closest, label='Closest Pair')
+plt.plot(test_case_n, runtimes_closest_dp, label='Closest Pair DP')
+plt.plot(test_case_n, runtimes_closest_rand, label='Closest Pair Random')
+
+plt.xlabel("Test Case Size")
+plt.ylabel("Runtime (seconds)")
+plt.legend()
+plt.title("Algorithm Runtimes")
+plt.grid()
+plt.show()
--- Homework_two/problems/problem_3/p3_b.py
+++ Homework_two/problems/problem_3/p3_b.py
@@ -1,41 +1,60 @@
-class Node: 
-    def __init__(self, val, level):
-        self.val = val
-        self.next = [None] * level
-        self.level = level
+import random
 
+class Node:
+  def __init__(self, val, level): 
+    self.val = val
+    self.next = [None] * level
 
 class SkipList:
-    def __init__(self, max_level, p):
-        self.max_level = max_level
-        self.p = p
-        self.sentinel = Node(None, self.max_level)
-        self.root = None
+  def __init__(self, max_level, p):
+    self.max_level = max_level
+    self.p = p 
+    self.root = Node(None, max_level)  # renamed from self.root to self.root
 
-    def random_level(self):
-        level = 0
-        while random.random() < self.p and level < self.max_level:
-            level += 1
-        return level
+  def random_level(self):
+    level = 0
+    while random.random() < self.p  and level < self.max_level - 1:  
+      level += 1
+    return min(level, self.max_level-1)
 
-    def insert(self, x: int) -> None:
-        new_level = self.random_level()
-        new_node = Node(x, self.max_level)  # change here, create node with max_level pointers
-        
-        update = [None] * (self.max_level+1)
-        current = self.root
+  def insert(self, x):
+    new_level = self.random_level()
+    new_node = Node(x, new_level)
+    update = [self.root] * self.max_level
 
-        for i in range(self.max_level, -1, -1):
-            while current and current.next[i] and current.next[i].val < x:
-                current = current.next[i]
-            update[i] = current
+    current = self.root
+    
+    for i in reversed(range(new_level + 1)):
+        while i < len(current.next) and current.next[i] and current.next[i].val < x: 
+            current = current.next[i]  
+        update[i] = current
 
-        if self.root is None or x < self.root.val:
-            for i in range(new_level+1):
-                new_node.next[i] = self.root
-            self.root = new_node
-        else:
-            for i in range(new_level+1):
-                if update[i] is not None:
-                    new_node.next[i] = update[i].next[i]
-                    update[i].next[i] = new_node
+    # If root's value is None (which means this is 
+    # the first element being inserted)
+    if self.root.val is None:
+        self.root = new_node
+        # Assign the next pointers of the root node
+        self.root.next = new_node.next
+    else:
+        for i in range(new_level+1):
+            if i >= len(new_node.next):
+                new_node.next.append(None)
+            
+            # Make this assignment only if `i` is within the range of `update[i].next`
+            if i < len(update[i].next):
+                new_node.next[i] = update[i].next[i]
+
+            # Here also check if `i` is within the range of `update[i].next`
+            if i < len(update[i].next):
+                update[i].next[i] = new_node
+
+  def search(self, x):
+    current = self.root
+    for i in reversed(range(self.max_level)):
+      while current.next[i] and current.next[i].val < x:
+        current = current.next[i]
+    current = current.next[0]
+    if current and current.val == x:
+      return current
+    else:
+      return None
