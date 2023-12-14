3a6b1d3d96261063a0f1a73977df622c5463f0c0
AmirFone <ahossain20@fordham.edu>
2023-12-14 14:23:17 -0500

    impamenting local seach for prob 3b

--- Homework_Four/problems/problem_3/p3_b.py
+++ Homework_Four/problems/problem_3/p3_b.py
@@ -1,27 +1,21 @@
 def calculate_cost(cost_matrix, sequence):
     cost = 0
     for i in range(len(sequence) - 1):
-        # Convert 1-based indices to 0-based indices
         cost += cost_matrix[sequence[i] - 1][sequence[i + 1] - 1]
     return cost
 
 def local_search_2opt(cost_matrix, candidate):
     candidate = list(candidate)  # Convert tuple to list for mutability
-    improved = True
-    while improved:
-        improved = False
-        best_cost = calculate_cost(cost_matrix, candidate)
+    best_candidate = candidate[:]
+    best_cost = calculate_cost(cost_matrix, candidate)
 
-        for i in range(len(candidate) - 1):
-            for j in range(i + 1, len(candidate)):
-                new_candidate = candidate[:]
-                # Reversed returns an iterator, convert it to a list
-                new_candidate[i:j + 1] = list(reversed(new_candidate[i:j + 1]))
-                new_cost = calculate_cost(cost_matrix, new_candidate)
+    for i in range(1, len(candidate) - 1):
+        for j in range(i + 1, len(candidate)):
+            new_candidate = candidate[:i] + candidate[i:j + 1][::-1] + candidate[j + 1:]
+            new_cost = calculate_cost(cost_matrix, new_candidate)
 
-                if new_cost < best_cost:
-                    candidate = new_candidate
-                    best_cost = new_cost
-                    improved = True
+            if new_cost < best_cost:
+                best_candidate = new_candidate[:]
+                best_cost = new_cost
 
-    return tuple(candidate[::-1])  # Convert back to tuple to match test expectation
+    return tuple(best_candidate) 
